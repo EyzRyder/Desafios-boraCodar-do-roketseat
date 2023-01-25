7dc5088ca324c1e7fb9a604f7dbb955c875f74d5
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2023-01-24 21:22:17 -0300

    css update

--- public/script.js
+++ public/script.js
@@ -8,8 +8,8 @@ fetch('../data.json')
         json.forEach((item) => {
             // console.log(item.link)
             desafios.innerHTML += `<a href="../${item.link}/index.html"> <div class="desafio"> 
-            <img src="${item.img}" alt="" />
-            <p>${item.link} - ${item.name}</p>
+            <img src="${item.img}" alt="desafio preview" />
+            <p class="pt-2">${item.link} - ${item.name}</p>
             </div>
             </a>
             `;

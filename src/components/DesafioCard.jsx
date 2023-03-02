8233de76611d7010d91a3617ce5df6bd504c3c6f
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2023-03-02 20:55:29 -0300

    update css

--- src/components/DesafioCard.jsx
+++ src/components/DesafioCard.jsx
@@ -9,11 +9,11 @@ export const DesafioCard = ({ url, rep, img, title, description }) => {
       <img src={img} alt={title} className="w-full h-[200px] object-contain" />
       <h1 className="text-center text-2xl font-medium py-1 ">{title}</h1>
       <p className=" w-full pt-2 flex-1">{description}</p>
-      <div className=" absolute bottom-[4%] left-[50%] translate-x-[-50%] flex gap-3">
-        <Link to={`desafio/${url}`} className="bg-purple-700 py-2 px-3 flex justify-center items-center rounded-lg">
+      <div className="absolute bottom-[4%] left-[50%] translate-x-[-50%] flex gap-3">
+        <Link to={`desafio/${url}`} className="bg-purple-700 py-2 px-3 flex justify-center items-center rounded-lg w-48">
           Site
         </Link>
-        <a href={rep} className="bg-purple-700 py-2 px-3 flex justify-center items-center rounded-lg">
+        <a href={rep} className="bg-purple-700 py-2 px-3 flex justify-center items-center rounded-lg w-48">
           Repository
         </a>
       </div>

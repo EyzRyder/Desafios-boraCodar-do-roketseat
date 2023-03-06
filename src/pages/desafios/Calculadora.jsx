5c1aa47df0bd5813da0a74d550d2e97cb545732d
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2023-03-05 23:21:16 -0300

    update functions

--- src/pages/desafios/Calculadora.jsx
+++ src/pages/desafios/Calculadora.jsx
@@ -1,14 +1,186 @@
 import { useState } from "react";
 
 export const Calculadora = () => {
-    const [operators, setOperators] = useState(["+", "-", "/", "*"]);
-    const [prev, setPrev] = useState();
-    const [inputBox, setInputBox] = useState();
+    const operators= ["+", "-", "/", "*"];
+    const [prev, setPrev] = useState("");
+    const [inputBox, setInputBox] = useState("");
     const [operatorValue, setOperatorValue] = useState();
-    const [lastButton, setLastButton] = useState();
     const [calcOperator, setCalcOperator] = useState();
+    const [lastOperator, setLastOperator] = useState();
+    const [lastButton, setLastButton] = useState("");
     const [firstNum, setFirstNum] = useState(true);
     const [numbers, setNumbers] = useState([]);
+    const [total, setTotal] = useState()
+    function pressBtn(btn) {
+        setLastButton(btn)
+
+        // if button is not an operator or = sign
+        if (!operators.includes(btn) && btn != '=') {
+            if (firstNum) {
+                // and it's a dot, show 0.
+                if (btn == '.') {
+                    setInputBox('0' + '.')
+                }
+                // else clear box and show the number
+                else {
+                    setInputBox(btn)
+                }
+                setFirstNum(false)
+            }
+            else {
+                // return if the box value is 0
+                if (inputBox.length === 1 && inputBox === 0) {
+                    if (btn === '.') {
+                        setInputBox(inputBox + btn)
+                    }
+                    return;
+                }
+                // return if the box already has a dot and clicked button is a dot
+                if (inputBox.includes('.') && btn === '.') {
+                    return;
+                }
+                // maximum allowed numbers inputted are 14
+                if (inputBox.length === 14) {
+                    return;
+                }
+
+                // if pressed dot and box already has a - sign, show -0.
+                if (btn == '.' && inputBox == "-") {
+                    setInputBox('-0' + '.')
+                }
+                // else append number
+                else {
+                    setInputBox(inputBox + btn)
+                }
+            }
+        }
+        // if it's an operator or = sign
+        else {
+
+            if (operatorValue != null && btn == operatorValue) {
+                return
+            }
+
+            // show minus sign if it's the first value selected and finally return
+            if (btn == "-" && inputBox == 0) {
+                setInputBox(btn)
+                setFirstNum(false)
+                setOperatorValue(btn)
+                return;
+            }
+            // return if minus operator pressed and it's already printed on screen 
+            else if (operators.includes(btn) && inputBox == "-") {
+                return
+            }
+            // return if minus operator pressed and history already has equal sign
+            else if (btn == "-" && operatorValue == "-" && prev.includes("=")) {
+                return
+            }
+
+            // set value of operator if it's one
+            if (operators.includes(btn)) {
+                if (typeof lastOperator != "undefined" && lastOperator != null) {
+                    setCalcOperator(lastOperator)
+                }
+                else {
+                    setCalcOperator(btn)
+                }
+                if (btn == "*") {
+                    setLastOperator("×")
+                }
+                else if (btn == "/") {
+                    setLastOperator("÷")
+                }
+                else {
+                    setLastOperator(btn)
+                }
+                setOperatorValue(btn)
+                setFirstNum(true)
+            }
+
+            // add first number to numbers array and show it on history
+            if (numbers.length == 0) {
+                setNumbers([...numbers, inputBox])
+                if (typeof lastOperator != 'undefined' && lastOperator != null) {
+                    setPrev(inputBox + ' ' + lastOperator)
+                }
+            }
+
+            // rest of calculations
+            else {
+                if (numbers.length == 1) {
+                    setNumbers([...numbers, inputBox])
+                }
+                var temp_num = inputBox
+
+                // calculate total
+                if (btn == '=' && calcOperator != null) {
+                    var value = calculate(numbers[0], numbers[1], calcOperator)
+                    setTotal(value)
+                    setInputBox(total);
+
+                    // append second number to history
+                    if (!prev.includes("=")) {
+                        setPrev(prev + ' ' + numbers[1] + ' ')
+                    }
+
+                    temp_num = numbers[0]
+
+                    setNumbers([total,...numbers])
+                    setOperatorValue(null)
+
+                    // replace first number of history with the value of total
+                    var history_arr = prev.split(" ")
+                    history_arr[0] = temp_num
+                    setPrev(history_arr.join(" "))
+                }
+                // update history with the value on screen and the pressed operator
+                else if (calcOperator != null) {
+                    setPrev(temp_num + " " + lastOperator)
+                    setCalcOperator(btn)
+                    setNumbers([])
+                    setNumbers([...numbers, inputBox])
+                }
+            }
+        }
+    }
+
+    function calculate(num1, num2, operator) {
+        let calc;
+        if (operator === "+") {
+            calc = (parseFloat)(num1) + (parseFloat)(num2)
+            setTotal(calc)
+        }
+        else if (operator === "-") {
+            calc = (parseFloat)(num1) - (parseFloat)(num2)
+            setTotal(calc)
+
+        }
+        else if (operator === "*") {
+            calc = (parseFloat)(num1) * (parseFloat)(num2)
+            setTotal(calc)
+
+        }
+        else if (operator === "/") {
+            calc = (parseFloat)(num1) / (parseFloat)(num2)
+            setTotal(calc)
+
+        }
+        else {
+            if (total == inputBox) {
+                return total
+            }
+            else {
+                return inputBox
+            }
+        }
+        // if total is not integer, show maximum 12 decimal places
+        if (!Number.isInteger(total)) {
+            calc = (parseFloat)(total).toPrecision(12)
+            setTotal(calc);
+        }
+        return parseFloat(total);
+    }
 
     function handlePercent() {
 
@@ -20,13 +192,7 @@ export const Calculadora = () => {
     function handleCEDelete() {
     }
 
-    function pressBtn(btn) {
 
-    }
-
-    function calculate(num1, num2, operator) {
-
-    }
 
     function handleDeleteAll() {
         window.location.reload()
@@ -118,7 +284,6 @@ export const Calculadora = () => {
                         </div>
                         <div
                             id="divide"
-                            value="/"
                             style={{
                                 boxShadow: '0px 11px 7px rgba(0, 0, 0, 0.01),0px 7px 7px rgba(0, 0, 0, 0.04), 0px 4px 6px rgba(0, 0, 0, 0.1), 0px 2px 4px rgba(0, 0, 0, 0.26), 0px 0px 2px rgba(0, 0, 0, 0.29), inset 0px 2px 3px rgba(255, 255, 255, 0.06)'
                             }}
@@ -135,7 +300,7 @@ export const Calculadora = () => {
                             boxShadow: '0px 11px 7px rgba(0, 0, 0, 0.01),0px 7px 7px rgba(0, 0, 0, 0.04),0px 4px 6px rgba(0, 0, 0, 0.1),0px 2px 4px rgba(0, 0, 0, 0.26),0px 0px 2px rgba(0, 0, 0, 0.29),inset 0px 2px 3px rgba(255, 255, 255, 0.06)'
                         }}
                             className="button rounded-full h-16 w-16 bg-[#2D2A37] bg-gradient-to-b from-[#00000005] to-[#ffffff05] text-[#EBEBEB] text-2xl text-center flex items-center justify-center transition-all ease-in-out cursor-pointer hover:bg-[#0f0e12] num"
-                            onClick={() => { pressBtn(7) }}>
+                            onClick={() => { pressBtn('7') }}>
                             7
                         </div>
                         <div
@@ -143,14 +308,14 @@ export const Calculadora = () => {
                                 boxShadow: '0px 11px 7px rgba(0, 0, 0, 0.01),0px 7px 7px rgba(0, 0, 0, 0.04),0px 4px 6px rgba(0, 0, 0, 0.1),0px 2px 4px rgba(0, 0, 0, 0.26),0px 0px 2px rgba(0, 0, 0, 0.29),inset 0px 2px 3px rgba(255, 255, 255, 0.06)'
                             }}
                             className="button rounded-full h-16 w-16 bg-[#2D2A37] bg-gradient-to-b from-[#00000005] to-[#ffffff05] text-[#EBEBEB] text-2xl text-center flex items-center justify-center transition-all ease-in-out cursor-pointer hover:bg-[#0f0e12] num"
-                            onClick={() => { pressBtn(8) }}>
+                            onClick={() => { pressBtn('8') }}>
                             8
                         </div>
                         <div style={{
                             boxShadow: '0px 11px 7px rgba(0, 0, 0, 0.01),0px 7px 7px rgba(0, 0, 0, 0.04),0px 4px 6px rgba(0, 0, 0, 0.1),0px 2px 4px rgba(0, 0, 0, 0.26),0px 0px 2px rgba(0, 0, 0, 0.29),inset 0px 2px 3px rgba(255, 255, 255, 0.06)'
                         }}
                             className="button rounded-full h-16 w-16 bg-[#2D2A37] bg-gradient-to-b from-[#00000005] to-[#ffffff05] text-[#EBEBEB] text-2xl text-center flex items-center justify-center transition-all ease-in-out cursor-pointer hover:bg-[#0f0e12] num"
-                            onClick={() => { pressBtn(9) }}>9</div>
+                            onClick={() => { pressBtn('9') }}>9</div>
                         <div
                             id="multi"
                             style={{
@@ -169,7 +334,7 @@ export const Calculadora = () => {
                                 boxShadow: '0px 11px 7px rgba(0, 0, 0, 0.01),0px 7px 7px rgba(0, 0, 0, 0.04),0px 4px 6px rgba(0, 0, 0, 0.1),0px 2px 4px rgba(0, 0, 0, 0.26),0px 0px 2px rgba(0, 0, 0, 0.29),inset 0px 2px 3px rgba(255, 255, 255, 0.06)'
                             }}
                             className="button rounded-full h-16 w-16 bg-[#2D2A37] bg-gradient-to-b from-[#00000005] to-[#ffffff05] text-[#EBEBEB] text-2xl text-center flex items-center justify-center transition-all ease-in-out cursor-pointer hover:bg-[#0f0e12] num"
-                            onClick={() => { pressBtn(4) }}
+                            onClick={() => { pressBtn('4') }}
                         >
                             4
                         </div>
@@ -177,7 +342,7 @@ export const Calculadora = () => {
                             boxShadow: '0px 11px 7px rgba(0, 0, 0, 0.01),0px 7px 7px rgba(0, 0, 0, 0.04),0px 4px 6px rgba(0, 0, 0, 0.1),0px 2px 4px rgba(0, 0, 0, 0.26),0px 0px 2px rgba(0, 0, 0, 0.29),inset 0px 2px 3px rgba(255, 255, 255, 0.06)'
                         }}
                             className="button rounded-full h-16 w-16 bg-[#2D2A37] bg-gradient-to-b from-[#00000005] to-[#ffffff05] text-[#EBEBEB] text-2xl text-center flex items-center justify-center transition-all ease-in-out cursor-pointer hover:bg-[#0f0e12] num"
-                            onClick={() => { pressBtn(5) }}
+                            onClick={() => { pressBtn('5') }}
                         >
                             5
                         </div>
@@ -185,13 +350,12 @@ export const Calculadora = () => {
                             boxShadow: '0px 11px 7px rgba(0, 0, 0, 0.01),0px 7px 7px rgba(0, 0, 0, 0.04),0px 4px 6px rgba(0, 0, 0, 0.1),0px 2px 4px rgba(0, 0, 0, 0.26),0px 0px 2px rgba(0, 0, 0, 0.29),inset 0px 2px 3px rgba(255, 255, 255, 0.06)'
                         }}
                             className="button rounded-full h-16 w-16 bg-[#2D2A37] bg-gradient-to-b from-[#00000005] to-[#ffffff05] text-[#EBEBEB] text-2xl text-center flex items-center justify-center transition-all ease-in-out cursor-pointer hover:bg-[#0f0e12] num"
-                            onClick={() => { pressBtn(6) }}
+                            onClick={() => { pressBtn('6') }}
                         >
                             6
                         </div>
                         <div
                             id="sub"
-                            value="-"
                             style={{
                                 boxShadow: '0px 11px 7px rgba(0, 0, 0, 0.01),0px 7px 7px rgba(0, 0, 0, 0.04),0px 4px 6px rgba(0, 0, 0, 0.1),0px 2px 4px rgba(0, 0, 0, 0.26),0px 0px 2px rgba(0, 0, 0, 0.29),inset 0px 2px 3px rgba(255, 255, 255, 0.06)'
                             }}
@@ -207,7 +371,7 @@ export const Calculadora = () => {
                             boxShadow: '0px 11px 7px rgba(0, 0, 0, 0.01),0px 7px 7px rgba(0, 0, 0, 0.04),0px 4px 6px rgba(0, 0, 0, 0.1),0px 2px 4px rgba(0, 0, 0, 0.26),0px 0px 2px rgba(0, 0, 0, 0.29),inset 0px 2px 3px rgba(255, 255, 255, 0.06)'
                         }}
                             className="button rounded-full h-16 w-16 bg-[#2D2A37] bg-gradient-to-b from-[#00000005] to-[#ffffff05] text-[#EBEBEB] text-2xl text-center flex items-center justify-center transition-all ease-in-out cursor-pointer hover:bg-[#0f0e12] num"
-                            onClick={() => { pressBtn(1) }}
+                            onClick={() => { pressBtn('1') }}
                         >
                             1
                         </div>
@@ -216,7 +380,7 @@ export const Calculadora = () => {
                                 boxShadow: '0px 11px 7px rgba(0, 0, 0, 0.01),0px 7px 7px rgba(0, 0, 0, 0.04),0px 4px 6px rgba(0, 0, 0, 0.1),0px 2px 4px rgba(0, 0, 0, 0.26),0px 0px 2px rgba(0, 0, 0, 0.29),inset 0px 2px 3px rgba(255, 255, 255, 0.06)'
                             }}
                             className="button rounded-full h-16 w-16 bg-[#2D2A37] bg-gradient-to-b from-[#00000005] to-[#ffffff05] text-[#EBEBEB] text-2xl text-center flex items-center justify-center transition-all ease-in-out cursor-pointer hover:bg-[#0f0e12] num"
-                            onClick={() => { pressBtn(2) }}
+                            onClick={() => { pressBtn('2') }}
                         >
                             2
                         </div>
@@ -225,13 +389,12 @@ export const Calculadora = () => {
                                 boxShadow: '0px 11px 7px rgba(0, 0, 0, 0.01),0px 7px 7px rgba(0, 0, 0, 0.04),0px 4px 6px rgba(0, 0, 0, 0.1),0px 2px 4px rgba(0, 0, 0, 0.26),0px 0px 2px rgba(0, 0, 0, 0.29),inset 0px 2px 3px rgba(255, 255, 255, 0.06)'
                             }}
                             className="button rounded-full h-16 w-16 bg-[#2D2A37] bg-gradient-to-b from-[#00000005] to-[#ffffff05] text-[#EBEBEB] text-2xl text-center flex items-center justify-center transition-all ease-in-out cursor-pointer hover:bg-[#0f0e12] num"
-                            onClick={() => { pressBtn(3) }}
+                            onClick={() => { pressBtn('3') }}
                         >
                             3
                         </div>
                         <div
                             id="sum"
-                            value="+"
                             style={{
                                 boxShadow: '0px 11px 7px rgba(0, 0, 0, 0.01),0px 7px 7px rgba(0, 0, 0, 0.04),0px 4px 6px rgba(0, 0, 0, 0.1),0px 2px 4px rgba(0, 0, 0, 0.26),0px 0px 2px rgba(0, 0, 0, 0.29),inset 0px 2px 3px rgba(255, 255, 255, 0.06)'
                             }}
@@ -261,7 +424,7 @@ export const Calculadora = () => {
                                 boxShadow: '0px 11px 7px rgba(0, 0, 0, 0.01),0px 7px 7px rgba(0, 0, 0, 0.04), 0px 4px 6px rgba(0, 0, 0, 0.1),0px 2px 4px rgba(0, 0, 0, 0.26), 0px 0px 2px rgba(0, 0, 0, 0.29),inset 0px 2px 3px rgba(255, 255, 255, 0.06)'
                             }}
                             className="button rounded-full h-16 w-16 bg-[#2D2A37] bg-gradient-to-b from-[#00000005] to-[#ffffff05] text-[#EBEBEB] text-2xl text-center flex items-center justify-center transition-all ease-in-out cursor-pointer hover:bg-[#0f0e12] num"
-                            onClick={() => { pressBtn(0) }}
+                            onClick={() => { pressBtn('0') }}
                         >
                             0
                         </div>
@@ -282,7 +445,6 @@ export const Calculadora = () => {
                             }}
                             className="button rounded-full h-16 w-16  bg-gradient-to-b from-[#00000005] to-[#ffffff05] text-[#EBEBEB] text-2xl text-center flex items-center justify-center transition-all ease-in-out cursor-pointer operator bg-[#7F45E2] hover:bg-[#0f0e12]"
                             onClick={() => { pressBtn("=") }}
-                            value="="
                         >
                             <img
                                 src="/svg/d5/Equals (1).svg"

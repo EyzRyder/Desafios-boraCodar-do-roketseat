d9f36cf41f82506bcba75ded9bdcbd365f025687
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2023-03-17 23:20:14 -0300

    update page

--- src/pages/desafios/ButaoCursores.jsx
+++ src/pages/desafios/ButaoCursores.jsx
@@ -1,66 +1,72 @@
+import { useRef } from 'react';
 import './ButaoCursores.scss'
 export const ButaoCursores = () => {
-  // document.querySelectorAll('.btn').forEach(item => {
 
-  //   item.addEventListener('dragstart', (e) => {
-  //     e.currentTarget.classList.add('dragging');
-  //     if (e.target.parentElement.className !== 'box') { return }
-  //     e.dataTransfer.setData("box", e.target.parentElement.id);
-  //     e.dataTransfer.setData("button", e.target.id);
-  //   });
+  const btn1 = useRef();
+  const btn2 = useRef();
+  const btn3 = useRef();
 
-  //   item.addEventListener('dragend', (e) => {
-  //     e.currentTarget.classList.remove('dragging');
-  //   });
+  document.querySelectorAll('.btn').forEach(item => {
 
-  //   item.addEventListener('click', (e) => {
-  //     if (e.currentTarget == document.querySelector('.btn.movable')) { return }
+    item.addEventListener('dragstart', (e) => {
+      e.currentTarget.classList.add('dragging');
+      if (e.target.parentElement.className !== 'box') { return }
+      e.dataTransfer.setData("box", e.target.parentElement.id);
+      e.dataTransfer.setData("button", e.target.id);
+    });
 
-  //     let btnCurrent = e.currentTarget;
-  //     btnCurrent.innerHTML = '<i class="fa fa-circle-o-notch fa-spin"></i> LOADING';
-  //     btnCurrent.classList.toggle('loader');
+    item.addEventListener('dragend', (e) => {
+      e.currentTarget.classList.remove('dragging');
+    });
 
-  //     setTimeout(() => {
-  //       btnCurrent.classList.toggle('loader');
+    item.addEventListener('click', (e) => {
+      if (e.currentTarget == document.querySelector('.btn.movable')) { return }
 
-  //       // made clickable the disable button
-  //       let oldDisabledBtn = document.querySelector(".btn.disabled");
-  //       oldDisabledBtn.removeAttribute("disabled");
-  //       oldDisabledBtn.classList.remove("disabled");
+      let btnCurrent = e.currentTarget;
+      btnCurrent.innerHTML = '<i class="fa fa-circle-o-notch fa-spin"></i> LOADING';
+      btnCurrent.classList.toggle('loader');
 
-  //       //disable movable button
-  //       let oldMovableBtn = document.querySelector(".btn.movable");
-  //       oldMovableBtn.innerHTML = 'INTERAJA COMIGO';
-  //       oldMovableBtn.classList.add("disabled");
-  //       oldMovableBtn.classList.remove("movable");
-  //       oldMovableBtn.setAttribute("draggable", false);
-  //       oldMovableBtn.setAttribute("disabled", true);
+      setTimeout(() => {
+        btnCurrent.classList.toggle('loader');
 
-  //       // made current button movable
-  //       btnCurrent.classList.add('movable');
-  //       btnCurrent.innerHTML = '<img src="/public/svg/d3/trelines.svg" alt="movable button"/> INTERAJA COMIGO';
-  //       btnCurrent.setAttribute("draggable", true);
-  //     }, 3000)
+        // made clickable the disable button
+        let oldDisabledBtn = document.querySelector(".btn.disabled");
+        oldDisabledBtn.removeAttribute("disabled");
+        oldDisabledBtn.classList.remove("disabled");
 
-  //   })
+        //disable movable button
+        let oldMovableBtn = document.querySelector(".btn.movable");
+        oldMovableBtn.innerHTML = 'INTERAJA COMIGO';
+        oldMovableBtn.classList.add("disabled");
+        oldMovableBtn.classList.remove("movable");
+        oldMovableBtn.setAttribute("draggable", false);
+        oldMovableBtn.setAttribute("disabled", true);
 
-  // });
+        // made current button movable
+        btnCurrent.classList.add('movable');
+        btnCurrent.innerHTML = '<img src="/public/svg/d3/trelines.svg" alt="movable button"/> INTERAJA COMIGO';
+        btnCurrent.setAttribute("draggable", true);
+      }, 3000)
 
-  // document.querySelectorAll('.box').forEach(item => {
-  //   item.addEventListener('dragover', (e) => {
-  //     e.preventDefault();
-  //   });
+    })
 
-  //   item.addEventListener('drop', (e) => {
+  });
 
-  //     let box = document.getElementById(e.dataTransfer.getData("box"));
-  //     let btn = document.getElementById(e.dataTransfer.getData("button"));
+  document.querySelectorAll('.box').forEach(item => {
+    item.addEventListener('dragover', (e) => {
+      e.preventDefault();
+    });
 
-  //     // if(){}
-  //     box.appendChild(e.currentTarget.firstElementChild);
-  //     e.currentTarget.appendChild(btn);
-  //   });
-  // });
+    item.addEventListener('drop', (e) => {
+
+      let box = document.getElementById(e.dataTransfer.getData("box"));
+      let btn = document.getElementById(e.dataTransfer.getData("button"));
+
+      // if(){}
+      box.appendChild(e.currentTarget.firstElementChild);
+      e.currentTarget.appendChild(btn);
+    });
+  });
 
   return (
     <div className="butaoCursores bg-[#130F1E] text-white min-h-screen">
@@ -69,21 +75,21 @@ export const ButaoCursores = () => {
 
       <div id="app" className="flex flex-col gap-8 p-11">
 
-        <div id="box1" className="box">
-          <button id="primary" draggable="false" className=" bg-[#8257E5] hover:bg-[#9674E5] btn">
+        <div id="box1" ref={btn1} className="box">
+          <button id="primary" draggable={false} className=" bg-[#8257E5] hover:bg-[#9674E5] btn">
             INTERAJA COMIGO
           </button>
         </div>
 
-        <div id="box2" className="box">
-          <button id="secondary" disabled="true" draggable="false"
+        <div id="box2" ref={btn2} className="box">
+          <button id="secondary" disabled={true} draggable="false"
             className=" bg-[#3C3748] hover:bg-[#6A617F] btn disabled">
             INTERAJA COMIGO
           </button>
         </div>
 
-        <div id="box3" className="box">
-          <button id="tertiary" draggable="true" className="btn movable">
+        <div id="box3" ref={btn3}  className="box">
+          <button id="tertiary" draggable={true} className="btn movable">
             <img src="/public/svg/d3/trelines.svg" alt="movable button" /> INTERAJA COMIGO
           </button>
         </div>
